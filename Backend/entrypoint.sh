3162701b5b85817775bad041ace0d4c06f8f0b08
fhernandez93 <fhdezale93@gmail.com>
2025-04-02 12:30:47 +0200

    Updated Actions

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Azure Login
-az login --service-principal -u "$CLIENT_ID" -p "$CLIENT_SECRET" --tenant "$TENANT_ID"
+# az login --service-principal -u "$CLIENT_ID" -p "$CLIENT_SECRET" --tenant "$TENANT_ID"
+az login
 
 # Start the Flask server
 exec python server.py
